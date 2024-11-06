347b4d75ab639a59639e9312480203f263e0e101
ahmed hossam <ahmed.hossam.elgamal77@gmail.com>
2024-11-07 00:23:53 +0200

    header desgin

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',redirectTo:'products',pathMatch:'full'},
+    {path:'products',loadComponent:()=>import('../app/components/layout/products/products.component').then((c)=>c.ProductsComponent),title:'products'},
+];
--- src/app/components/layout/products/products.component.ts
+++ src/app/components/layout/products/products.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../header/header.component";
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss'
 })
