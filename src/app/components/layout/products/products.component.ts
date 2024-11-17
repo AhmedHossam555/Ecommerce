06f724d3bf3f303a957e705bc09802472343b156
ahmed hossam <ahmed.hossam.elgamal77@gmail.com>
2024-11-17 02:41:47 +0200

    wish list icon

--- src/app/components/layout/products/products.component.ts
+++ src/app/components/layout/products/products.component.ts
@@ -1,15 +1,12 @@
-import { Component, EventEmitter, inject, OnInit, Output, signal, WritableSignal } from '@angular/core';
-import { HeaderComponent } from "../header/header.component";
-import { MainLayoutComponent } from "../main-layout/main-layout.component";
+import { Component, inject, OnInit, signal, WritableSignal } from '@angular/core';
 import { ProductService } from '../../../shared/services/product.service';
 import { Product } from '../../../shared/interfaces/product';
 import { CurrencyPipe } from '@angular/common';
 
-
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [HeaderComponent, MainLayoutComponent, CurrencyPipe],
+  imports: [CurrencyPipe],
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss'
 })
@@ -20,12 +17,24 @@ export class ProductsComponent implements OnInit {
   ngOnInit(): void {
     this.getALlProduct();
   }
+  addToWishList(event:Event){
+    const icon = event.target as HTMLElement;
+    const parent = icon.parentElement as HTMLElement;
+    if(parent.classList.contains('fav')){
+      parent.classList.remove('fav');
+    }else{
+      parent.classList.add('fav');
+    }
+  }
 
   getALlProduct(){
     this._ProductService.getProduct().subscribe({
       next: (res)=>{
         console.log(res);
         this.products.set(res);
+      },
+      error: (err)=>{
+        console.log(err);
       }
     })
   }
