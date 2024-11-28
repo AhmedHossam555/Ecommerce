490557b9429521a6695715e04952fcd130bd49b8
ahmed hossam <ahmed.hossam.elgamal77@gmail.com>
2024-11-28 20:14:42 +0200

    reactive form and error message in login

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -1,16 +1,32 @@
 import { CommonModule } from '@angular/common';
-import { Component, signal, WritableSignal } from '@angular/core';
+import { Component, inject, signal, WritableSignal } from '@angular/core';
+import { UserService } from '../../../shared/services/user/user.service';
+import { Router, RouterLink } from '@angular/router';
+import { HotToastService } from '@ngneat/hot-toast';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Login } from '../../../shared/interfaces/login';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,RouterLink,ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  private _UserService = inject(UserService);
+  private _Router = inject(Router);
+  private _toastr = inject(HotToastService);
   isVisisble:WritableSignal<boolean> = signal(false);
 
+  loginForm:FormGroup = new FormGroup({
+    email: new FormControl(null, [Validators.required,Validators.email]),
+    password: new FormControl(null, [Validators.required, Validators.minLength(5)]),
+  })
+  onSubmit(){
+    this.loginForm.markAllAsTouched;
+    console.log(this.loginForm.value)
+  }
   visiableControl(){
     this.isVisisble.update((val)=> val =  !val);
     console.log(this.isVisisble())
--- src/app/components/auth/register/register.component.ts
+++ src/app/components/auth/register/register.component.ts
@@ -2,19 +2,23 @@ import { CommonModule } from '@angular/common';
 import { Component, inject, signal, WritableSignal } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { UserService } from '../../../shared/services/user/user.service';
+import { HotToastService } from '@ngneat/hot-toast';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [CommonModule,ReactiveFormsModule],
+  imports: [CommonModule,ReactiveFormsModule,RouterLink],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
   private _UserService = inject(UserService);
-  
+  private _toast  = inject(HotToastService);
+  private _Route = inject(Router);
   isVisiable:WritableSignal<boolean> = signal(false);
   errorMsg: WritableSignal<string[]> = signal([]);
+
   registerForm:FormGroup = new FormGroup({
     name: new FormControl(null, [Validators.required,Validators.minLength(5)]),
     email: new FormControl(null,[Validators.required, Validators.email]),
@@ -23,16 +27,22 @@ export class RegisterComponent {
   })
   onSubmit(){
     this.registerForm.markAllAsTouched()
-    this._UserService.register(this.registerForm.value).subscribe({
+    this._UserService.register(this.registerForm.value).pipe(
+      this._toast.observe({
+        loading: 'Logging in',
+        success:' Congrats ! You are registered',
+        error: (err)=>`There was an error: ${err.message}`
+      })
+    ).subscribe({
       next: (res)=>{
-        console.log(res)
+       if(res){
+        this._Route.navigate(['/login']);
+       }
       },
       error: (err)=>{
-        console.log(err.error.message);
         this.errorMsg.set(err.error.message);
       }
     })
- 
   }
   visiableControl(){
     this.isVisiable.update((val)=> val = !val);
