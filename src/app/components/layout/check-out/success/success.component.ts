379acc4b773ceff3cb469c7a6d26464c5650d3d9
ahmed hossam <ahmed.hossam.elgamal77@gmail.com>
2024-11-30 23:33:33 +0200

    success style

--- src/app/components/layout/check-out/success/success.component.ts
+++ src/app/components/layout/check-out/success/success.component.ts
@@ -1,19 +1,22 @@
 import { Component, inject, signal, WritableSignal } from '@angular/core';
 import { CartService } from '../../../../shared/services/cart/cart.service';
 import { HeaderComponent } from "../../header/header.component";
-import { CurrencyPipe } from '@angular/common';
+import { CurrencyPipe, DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-success',
   standalone: true,
-  imports: [HeaderComponent,CurrencyPipe],
+  imports: [HeaderComponent,CurrencyPipe,DatePipe],
   templateUrl: './success.component.html',
   styleUrl: './success.component.scss'
 })
 export class SuccessComponent {
   private _CartService = inject(CartService);
   total: WritableSignal<number> = signal(0);
+  Data: WritableSignal<any> = signal({})
+ 
   ngOnInit() {
     this.total.set( this._CartService.totalPrice());
+    this.Data.update((val) => val = new Date());
   }
 }
