4d36b93857a60c6fe3166706fc0e02fe97e5a23c
ahmed hossam <ahmed.hossam.elgamal77@gmail.com>
2024-11-30 20:51:06 +0200

    reactive form billing

--- src/app/components/layout/check-out/check-out.component.ts
+++ src/app/components/layout/check-out/check-out.component.ts
@@ -1,23 +1,13 @@
-import { Component, inject, signal, WritableSignal } from '@angular/core';
-import { NgxSpinnerModule, NgxSpinnerService } from "ngx-spinner";
-import { CartService } from '../../../shared/services/cart/cart.service';
-import { Product } from '../../../shared/interfaces/product';
+import { Component } from '@angular/core';
+import { OrderComponent } from "./order/order.component";
 
 @Component({
   selector: 'app-check-out',
   standalone: true,
-  imports: [],
+  imports: [OrderComponent],
   templateUrl: './check-out.component.html',
   styleUrl: './check-out.component.scss'
 })
 export class CheckOutComponent {
- private _CartService = inject(CartService);
- products: WritableSignal<Product[]> = signal([])
- ngOnInit() {
-  this._CartService.cartProduct.subscribe({
-    next: (res)=> {
-      console.log(res)
-    }
-  })
- }
+
 }
--- src/app/components/layout/check-out/order/order.component.ts
+++ src/app/components/layout/check-out/order/order.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal, WritableSignal } from '@angular/core';
+import { Product } from '../../../../shared/interfaces/product';
+import { CartService } from '../../../../shared/services/cart/cart.service';
+import { HeaderComponent } from "../../header/header.component";
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-order',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent,ReactiveFormsModule],
   templateUrl: './order.component.html',
   styleUrl: './order.component.scss'
 })
 export class OrderComponent {
+  private _CartService = inject(CartService);
+  products: WritableSignal<Product[]> = signal([])
+  ngOnInit() {
+   this._CartService.cartProduct.subscribe({
+     next: (res)=> {
+       this.products.set(res)
+     }
+   })
+  }
+  changeRadio($event:Event){
+    document.querySelectorAll(".box").forEach((ele)=>{
+      ele.classList.remove('active')
+    })
+    const ele = $event.currentTarget as HTMLElement;
+    ele.classList.add('active');
+    const input = ele.childNodes[0] as HTMLInputElement;
+    input.checked = true;
+  }
+  bilingForm = new FormGroup({
+    first: new FormControl(null, Validators.required),
+    last: new FormControl(null, Validators.required),
+    phone: new FormControl(null, [Validators.required]),
+    email: new FormControl(null, [Validators.required, Validators.email]),
+  })
 
 }
