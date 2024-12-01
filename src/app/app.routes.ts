e25a7065bf08be1e7710104d1cace40fdd05c1ae
ahmed hossam <ahmed.hossam.elgamal77@gmail.com>
2024-12-01 03:26:08 +0200

    add guard to routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,8 +9,8 @@ export const routes: Routes = [
     {path:'user/wishlist',loadComponent:()=>import('../app/components/layout/user/wishlist/wishlist.component').then((c)=> c.WishlistComponent),canActivate:[authGuard],title:'wishlist'},
     {path:'login',loadComponent:()=>import('../app/components/auth/login/login.component').then((c)=>c.LoginComponent),title:'Login'},
     {path:'register',loadComponent:()=>import('../app/components/auth/register/register.component').then((c)=>c.RegisterComponent),title:'register'},
-    {path:'checkout',loadComponent: ()=> import('../app/components/layout/check-out/check-out.component').then((c)=>c.CheckOutComponent),title:'checkout'},
-    {path: 'checkout/success',loadComponent: ()=> import('../app/components/layout/check-out/success/success.component').then((c)=> c.SuccessComponent),title:'success'},
+    {path:'checkout',loadComponent: ()=> import('../app/components/layout/check-out/check-out.component').then((c)=>c.CheckOutComponent),canActivate:[authGuard],title:'checkout'},
+    {path: 'checkout/success',loadComponent: ()=> import('../app/components/layout/check-out/success/success.component').then((c)=> c.SuccessComponent),canActivate:[authGuard],title:'success'},
     {path:'not-found',loadComponent: ()=> import('../app/components/layout/error-page/error-page.component').then((c)=>c.ErrorPageComponent),title: 'not-found'},
     {path:'**', redirectTo:'not-found',pathMatch:'full'},
 ];
