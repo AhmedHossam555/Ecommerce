95190b684e78c1e4550fa104e4f8cfef993d54f3
ahmed hossam <ahmed.hossam.elgamal77@gmail.com>
2024-12-15 20:04:38 +0200

    update change

--- src/app/shared/guard/auth.guard.ts
+++ src/app/shared/guard/auth.guard.ts
@@ -5,7 +5,6 @@ import { UserService } from '../services/user/user.service';
 export const authGuard: CanActivateFn = (route, state) => {
   const _UserService = inject(UserService);
   const _Router = inject(Router);
-  console.log(_UserService.userInformation.getValue())
   if(_UserService.userInformation.getValue() !== null){
     return true;
   }else{
