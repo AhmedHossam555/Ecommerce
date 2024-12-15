715ed2f052def288f887507c7dfec75357b849aa
ahmed hossam <ahmed.hossam.elgamal77@gmail.com>
2024-12-15 10:56:24 +0200

    update user service

--- src/app/shared/services/user/user.service.ts
+++ src/app/shared/services/user/user.service.ts
@@ -1,10 +1,11 @@
 import { HttpClient } from '@angular/common/http';
-import { afterNextRender, inject, Injectable } from '@angular/core';
+import { afterNextRender, inject, Injectable, PLATFORM_ID } from '@angular/core';
 import { BehaviorSubject, Observable, of, switchMap } from 'rxjs';
 import { Enviroment } from '../../Enviroment/enviroment';
 import { Login } from '../../interfaces/login';
 import { Register } from '../../interfaces/register';
 import { jwtDecode } from 'jwt-decode';
+import { isPlatformBrowser } from '@angular/common';
 export interface TokenPayload {
   exp: number;
   iat: number;
@@ -15,26 +16,45 @@ export interface TokenPayload {
 })
 export class UserService {
   private _HttpClient = inject(HttpClient);
+  private _plat = inject(PLATFORM_ID);
   private refreshTokenTimeOut:any;
   private token:string | null = null;
   userInformation = new BehaviorSubject<any>(null);
+  constructor(){
+    if(isPlatformBrowser(this._plat)){
+      this.loadToken();
+    }
+  }
 
-  ngOnInit() {
-    this.userToken();
-  }
-  login(loginForm:Login):Observable<any>{
-    return this._HttpClient.post(`${Enviroment.baseUrl}/auth/login`,loginForm)
-  }
+  // login(loginForm:Login):Observable<any>{
+  //   return this._HttpClient.post(`${Enviroment.baseUrl}/auth/login`,loginForm)
+  // }
   register(registerForm:Register):Observable<any> {
     return this._HttpClient.post(`${Enviroment.baseUrl}/users/`,registerForm)
   }
   loadToken(){
-    this.token = JSON.stringify(window.localStorage.getItem('token'));
+    this.token = JSON.stringify(window.localStorage.getItem('access_token'));
     if(this.token){
       this.decodedToken(this.token);
-
+      this.ScheduleTokenRefresh();
     }
 
+  }
+
+  login(loginForm:Login):Observable<any>{
+    return this._HttpClient.post(`${Enviroment.baseUrl}/auth/login`,loginForm).pipe(
+      switchMap((response:any)=>{
+        this.storeToken(response.access_token, response.refresh_token);
+        this.decodedToken(response.access_token);
+        this.ScheduleTokenRefresh();
+        return of(response);
+      })
+    )
+  }
+  storeToken(accessToken:string,refreshToken:string){
+    window.localStorage.setItem('access_token', accessToken );
+    window.localStorage.setItem('refresh_token', refreshToken);
+    this.token = accessToken;
   }
   private decodedToken(token:string){
     try{
@@ -50,24 +70,6 @@ export class UserService {
       console.log("Invalid token",error);
       this.clearSession();
     }
-
-  }
-  LoginForm(loginForm:Login):Observable<any>{
-    return this._HttpClient.post(`${Enviroment.baseUrl}/auth/login`,loginForm).pipe(
-      switchMap((response:any)=>{
-        this.storeToken(response.access_token, response.refresh_token);
-        this.decodedToken(response.access_token);
-
-        return of(response);
-      })
-    )
-  }
-
-
-  storeToken(accessToken:string,refreshToken:string){
-    window.localStorage.setItem('access_token', accessToken );
-    window.localStorage.setItem('refresh_token', refreshToken);
-    this.token = accessToken;
   }
   private ScheduleTokenRefresh(){
     if(!this.token) return;
@@ -85,6 +87,8 @@ export class UserService {
   refreshToken():Observable<any>{
     const refreshToken = window.localStorage.getItem('refresh_token');
     if(!refreshToken){
+      console.warn("no refresh token available");
+      this.clearSession();
       return of(null)
     }
     return this._HttpClient.post(`${Enviroment.baseUrl}/auth/refresh-token`,refreshToken).pipe(
@@ -97,7 +101,7 @@ export class UserService {
     )
     
   }
-  private clearSession(){
+  clearSession(){
     window.localStorage.removeItem('access_token');
     window.localStorage.removeItem('refresh_token');
     window.clearTimeout(this.refreshTokenTimeOut);
@@ -107,19 +111,18 @@ export class UserService {
   getToken(): string | null {
     return this.token;
   }
-  userToken(){
-    if(window.localStorage.getItem('token')){
-      const token = JSON.stringify(window.localStorage.getItem('token'));
-      const decoded = jwtDecode(token);
-      console.log(decoded);
-      this.userInformation.next(decoded);
-    }
-  
-  }
+  // userToken(){
+  //   if(window.localStorage.getItem('token')){
+  //     const token = JSON.stringify(window.localStorage.getItem('token'));
+  //     const decoded = jwtDecode(token);
+  //     console.log(decoded);
+  //     this.userInformation.next(decoded);
+  //   }
+  // }
   getProfile():Observable<any>{
     return this._HttpClient.get(`${Enviroment.baseUrl}/auth/profile`,{
       headers:{
-        "Authorization": `Bearer ${window.localStorage.getItem('token')!}`,
+        "Authorization": `Bearer ${window.localStorage.getItem('access_token')!}`,
       }
     })
   }
